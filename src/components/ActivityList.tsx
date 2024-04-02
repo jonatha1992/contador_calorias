cc67cb1f7a17ab3bbdecccd0651207891c011f40
jonathan correa <jonathancorrea291292@hotmail.com>
2024-04-02 14:38:07 -0300

    cambios de activity list

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,8 +1,9 @@
 import { categories } from "../data/categories";
 import useActivity from "../hooks/useActivity";
+import { PencilSquareIcon, TrashIcon } from "@heroicons/react/24/outline";
 
 export default function ActivityList() {
-    const { state } = useActivity();
+    const { state, dispatch } = useActivity();
 
     const getCategoryNameById = (categoryId: number) => {
         const category = categories.find((cat) => cat.id === categoryId);
@@ -12,20 +13,40 @@ export default function ActivityList() {
         return category.name;
     };
 
+    const handleEdit = (id: string) => {
+        dispatch({ type: "set-activeId", payload: { id } });
+    };
+
+    const handleDelete = (id: string) => {
+        dispatch({ type: "delete-activity", payload: { id } });
+    };
+
     return (
         <>
             <h2 className="text-center font-black text-2xl mb-5">Comida y Actividades</h2>
 
             {state.activities.map((activity) => (
-                <div key={activity.id} className=" mx-auto bg-slate-200 p-5 flex flex-col shadow-lg justify-between ">
-                    <div className="space-y-3 relative">
-                        <p className=" text-2xl font-bold capitalize ">{getCategoryNameById(activity.category)}</p>
+                <div key={activity.id} className=" mx-auto bg-slate-200 p-5 flex  shadow-lg justify-between ">
+                    <div className="space-y-3 relative justify-between">
+                        <p
+                            className={`absolute uppercase text-white -top-8 -left-8 px-10 py-2 ${
+                                activity.category === 1 ? "bg-orange-600" : "bg-green-600"
+                            }`}
+                        >
+                            {getCategoryNameById(activity.category)}
+                        </p>
                         <p className=" text-2xl font-bold capitalize "> {activity.name}</p>
-                        <p className=" text-4xl font-black capitalize text-lime-600 "> {activity.calories} calorias</p>
+                        <p className=" text-4xl font-black capitalize text-lime-500 "> {activity.calories} calorias</p>
                     </div>
-                    <div>
-                        <button> Editar</button>
-                        <button> Eliminar</button>
+                    <div className="flex items-center gap-5">
+                        <button onClick={() => handleEdit(activity.id)}>
+                            {" "}
+                            <PencilSquareIcon className="w-6 h-6" />
+                        </button>
+                        <button onClick={() => handleDelete(activity.id)}>
+                            {" "}
+                            <TrashIcon className="w-6 h-6" />
+                        </button>
                     </div>
                 </div>
             ))}
--- src/components/CalorieTracker.tsx
+++ src/components/CalorieTracker.tsx
@@ -1,21 +1,21 @@
 import useActivity from "../hooks/useActivity";
 
 export default function CalorieTracker() {
-    const { state } = useActivity();
+    const { caloriesBurned, caloriesConsumed } = useActivity();
     return (
         <>
             <h2 className="text-center font-bold text-2xl text-white mb-10 ">Resumen de calorias</h2>
             <div className="flex flex-col  md:flex-row md:justify-between gap-20  ">
                 <div>
-                    <p className="text-center font-bold text-6xl text-white">0</p>
+                    <p className="text-center font-bold text-6xl text-white">{caloriesConsumed}</p>
                     <p className="text-center font-bold text-1xl text-white">Consumidas</p>
                 </div>
                 <div>
-                    <p className="text-center font-bold text-6xl text-white">0</p>
+                    <p className="text-center font-bold text-6xl text-white">{caloriesBurned}</p>
                     <p className="text-center font-bold text-1xl text-white">Ejercicio</p>
                 </div>
                 <div>
-                    <p className="text-center font-bold text-6xl text-white">0</p>
+                    <p className="text-center font-bold text-6xl text-white">{caloriesConsumed - caloriesBurned}</p>
                     <p className="text-center font-bold text-1xl text-white">Diferencia</p>
                 </div>
             </div>
